a1846fa61efc195e5dd6512243c90e5ca0b00cc0
AryanGKulkarni <95529889+AryanGKulkarni@users.noreply.github.com>
2025-04-13 00:07:18 +0530

    database and swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,35 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'postgres', // matches docker service name
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'aum-erp',
-      autoLoadModels: true,
-      synchronize: true, // disable in production
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadModels: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
   ],
   controllers: [AppController],
