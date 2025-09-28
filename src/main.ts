d8b56e36080bf81df1012ae22fcae7900d20c372
Aryan Kulkarni <aryangkulkarni@gmail.com>
2025-09-28 13:52:22 +0530

    fixed cors errors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { Sequelize } from 'sequelize-typescript';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true
+  });
 
   app.setGlobalPrefix('api');
 
